325e6633637a0e910c810da5a6430051c0489e40
popemray00 <popemray1986@gmail.com>
2019-08-11 13:13:02 -0700

    navbar complete

--- client/src/components/Stylist.js
+++ client/src/components/Stylist.js
@@ -4,7 +4,22 @@ class Stylist extends React.Component {
     render() {
       return (
         <div>
-            Stylist Page
+            <div className="media border p-3">
+              <img src="images/salonhome.png" alt="John Doe" className="mr-3 mt-3 rounded-circle" />
+              <div className="media-body">
+                <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
+                <p>Lorem ipsum...</p>
+              </div>
+            </div>
+
+            <div class="media">
+              <img src="images/salonhome.png" class="align-self-center mr-3" />
+              <div class="media-body">
+                <h4>Media Middle</h4>
+                <p>Lorem ipsum...</p>
+              </div>
+            </div>
+
         </div>
       )
     }
